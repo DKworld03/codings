83fc0f6c67ce56c81ea202e5dfcca1658aea7bc0
Divanshu kumawat <kumawatdivanshu96@gmail.com>
2025-08-03 16:28:19 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the first value:");
+    scanf("%d",&a);
+    printf("enter the second value:");
+    scanf("%d",&b);
+    c=a+b;
+    printf("the addition is:%d",c);
+    return 0;
+
+}
