e63fb6f4e9fc32e6072b541aa12afe67d9d96e5c
Divanshu kumawat <kumawatdivanshu96@gmail.com>
2025-08-03 16:39:47 +0530

    Add files via upload

---
+++ division.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter first value for division:");
+    scanf("%d",&a);
+    printf("enter second value:");
+    scanf("%d",&b);
+    c=a/b;
+    printf("the final value is:%d",c);
+    return 0;
+}
