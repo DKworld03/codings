69a0b7f4b71f278a160fba9e9a8dad601c83ee36
Divanshu kumawat <kumawatdivanshu96@gmail.com>
2025-08-03 16:34:13 +0530

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the first value:");
+    scanf("%d",&a);
+    printf("enter the second value:");
+    scanf("%d",&b);
+    c=a-b;
+    printf("your subtracted value is:%d",c);
+    return 0;
+}
