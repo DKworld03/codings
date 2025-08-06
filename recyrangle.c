ac24e6c9f17469bd1c10405712161f193d6acd86
Divanshu kumawat <kumawatdivanshu96@gmail.com>
2025-08-06 12:27:16 +0530

    Add files via upload

---
+++ recyrangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+    int a,b;
+    printf("enter lenght of rectrangle:");
+    scanf("%d",&a);
+    printf("enter breadth of rectrangle:");
+    scanf("%d",&b);
+    printf("area of rec is:%d sqm",a*b);
+    return 0;
+}
