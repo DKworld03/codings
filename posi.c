8c3493d8eaf7b950e570cc39167b2548b92fb9b7
Divanshu kumawat <kumawatdivanshu96@gmail.com>
2025-08-06 12:26:56 +0530

    Add files via upload

---
+++ posi.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main ()
+{
+
+    int a;
+    printf("enter a number:");
+    scanf("%d",&a);
+    if(a>0)
+    {
+
+        printf("given number positive");
+    }
+    else
+        {
+        printf("given number negitive");
+
+    }
+    return 0;
+    }
+
