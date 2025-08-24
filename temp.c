61fb688af22a58fd8e7fe1b54341dc131e957c48
Divanshu kumawat <kumawatdivanshu96@gmail.com>
2025-08-24 15:59:08 +0530

    Add files via upload

---
+++ temp.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float F, C, K;
+    printf("enter temperture in Fahrenheit: ");
+    scanf("%f", &F);
+    printf("\ntemperture in Celsius:%f", (5.0/9.0 * (F - 32)));
+    C=(5.0/9.0 * (F - 32));
+     printf("\ntemperture in Kelvin:%f", C + 273.15);
+   return 0;
+}
