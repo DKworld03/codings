59d8e9e35746c4576b5b65d89c6ef1d99bf4ec57
Divanshu kumawat <kumawatdivanshu96@gmail.com>
2025-08-24 17:50:44 +0530

    Add files via upload

---
+++ threeD.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    float h , b , l ;
+    printf("enter height for cuboid : ");
+    scanf("%f",&h);
+    printf("enter length for cuboid : ");
+    scanf("%f",&l);
+    printf("enterbreath for cuboid : ");
+    scanf("%f",&b);
+    printf("surface area  fof cuboid is  :%f",2*(l*b+l*h+b*h));
+    printf("\nvolume of cubiod is  :%f",l*b*h);
+    return 0;
+}
+
