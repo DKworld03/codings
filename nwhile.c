41e262a99e927d29c98edfd40f259be85153e791
Divanshu kumawat <kumawatdivanshu96@gmail.com>
2025-09-08 09:36:44 +0530

    Add files via upload

---
+++ nwhile.c
@@ -0,0 +1,19 @@
+//Q.to accept a number n and print value from 1 to n
+
+#include <stdio.h>
+int main()
+{
+    printf("Roll no:23||Divanshu kumawat");
+    int i, n;
+    i = 1;
+    printf("\nEnter n value:");
+    scanf("%d", &n);
+    while (i <= n)
+    {
+        printf("\n%d", i);
+        i = i + 1;
+    }
+    printf("\ncode ended");
+    printf("\nRoll no:23||Divanshu kumawat");
+    return 0;
+}
