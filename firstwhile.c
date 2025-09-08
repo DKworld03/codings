bc4b89f42051b7b9c2f4be83695e41b87838579b
Divanshu kumawat <kumawatdivanshu96@gmail.com>
2025-09-08 09:38:17 +0530

    Add files via upload

---
+++ firstwhile.c
@@ -0,0 +1,16 @@
+//Q.print number up to 10.
+
+#include <stdio.h>
+int main()
+{
+    printf("Roll no:23||Divanshu kumawat");
+    int i;
+    i = 1;
+    while (i <= 10)
+    {
+        printf("\n%d.JAI hind", i);
+        i = i + 1;
+    }
+    printf("\nRoll no:23||Divanshu kumawat");
+    return 0;
+}
