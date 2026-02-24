9902b76d45b1ae2704660a2fe1ab28332fef54ce
DKworld03 <kumawatdivanshu96@gmail.com>
2026-02-24 19:47:34 +0530

    calculating length of string

--- threeD.c
+++ threeD.c
@@ -4,9 +4,9 @@ int main()
     float h , b , l ;
     printf("enter height for cuboids: ");
     scanf("%f",&h);
-    printf("enter length for cuboid : ");
+    printf("enter length for cuboids : ");
     scanf("%f",&l);
-    printf("enterbreath for cuboid : ");
+    printf("enterbreath for cuboids : ");
     scanf("%f",&b);
     printf("surface area  fof cuboid is  :%f",2*(l*b+l*h+b*h));
     printf("\nvolume of cubiod is  :%f",l*b*h);
