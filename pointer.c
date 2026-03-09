6b8c757e0a4d57cd9a08d542fe3204fd32c2f7b3
DKworld03 <kumawatdivanshu96@gmail.com>
2026-03-09 18:20:40 +0530

    pointer

---
+++ pointer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 10;
+    int *p;
+
+    p = &a;
+
+    printf("Value of a = %d\n", a);
+    printf("Address of a = %p\n", &a);
+    printf("Pointer value = %p\n", p);
+    printf("Value using pointer = %d", *p);
+
+    return 0;
+}
